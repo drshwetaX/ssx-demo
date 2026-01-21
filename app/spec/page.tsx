2ae5b73b825cc8d3f2735d6b3640949d9ac29350
drss <drshweta.shah5@gmail.com>
2026-01-21 11:27:04 -0500

    Add Page component with Shell and SpecView

---
+++ app/spec/page.tsx
@@ -0,0 +1,5 @@
+import Shell from "@/components/Shell";
+import DemoProvider from "@/components/DemoProvider";
+import SpecView from "@/components/views/SpecView";
+
+export default function Page(){return(<DemoProvider><Shell><SpecView/></Shell></DemoProvider>)}
