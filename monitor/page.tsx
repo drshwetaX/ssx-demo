e40194f63a5b57692c519ca0b6610eb58ac1f588
drss <drshweta.shah5@gmail.com>
2026-01-21 11:24:05 -0500

    Delete monitor directory

--- monitor/page.tsx
+++
@@ -1,5 +0,0 @@
-import Shell from "@/components/Shell";
-import DemoProvider from "@/components/DemoProvider";
-import MonitorView from "@/components/views/MonitorView";
-
-export default function Page(){return(<DemoProvider><Shell><MonitorView/></Shell></DemoProvider>)}
