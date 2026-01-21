8e65dffc8761a874ccd44dbcdbc42d2815501178
drss <drshweta.shah5@gmail.com>
2026-01-21 11:29:25 -0500

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,5 @@
+import Shell from "@/components/Shell";
+import DemoProvider from "@/components/DemoProvider";
+import HomeView from "@/components/views/HomeView";
+
+export default function Page(){return(<DemoProvider><Shell><HomeView/></Shell></DemoProvider>)}
