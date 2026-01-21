7a5152beeadff6a12815b7ed5870074b932ae368
drss <drshweta.shah5@gmail.com>
2026-01-21 11:24:28 -0500

    Delete spec directory

--- spec/page.tsx
+++
@@ -1,5 +0,0 @@
-import Shell from "@/components/Shell";
-import DemoProvider from "@/components/DemoProvider";
-import SpecView from "@/components/views/SpecView";
-
-export default function Page(){return(<DemoProvider><Shell><SpecView/></Shell></DemoProvider>)}
