527ae592e2373bfe57770e95a931be7269a07808
drss <drshweta.shah5@gmail.com>
2026-01-21 11:09:48 -0500

    Add Monitor page with Shell and DemoProvider

---
+++ monitor/page.tsx
@@ -0,0 +1,5 @@
+import Shell from "@/components/Shell";
+import DemoProvider from "@/components/DemoProvider";
+import MonitorView from "@/components/views/MonitorView";
+
+export default function Page(){return(<DemoProvider><Shell><MonitorView/></Shell></DemoProvider>)}
