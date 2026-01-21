d6cd871ba049a8667b8700b53b637b108869ada0
drss <drshweta.shah5@gmail.com>
2026-01-21 11:10:45 -0500

    Add Page component with Shell and RegistryView

---
+++ registry/page.tsx
@@ -0,0 +1,5 @@
+import Shell from "@/components/Shell";
+import DemoProvider from "@/components/DemoProvider";
+import RegistryView from "@/components/views/RegistryView";
+
+export default function Page(){return(<DemoProvider><Shell><RegistryView/></Shell></DemoProvider>)}
