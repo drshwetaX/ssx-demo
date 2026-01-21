4acebc48040d173e5a662aa2821742e10b64334d
drss <drshweta.shah5@gmail.com>
2026-01-21 14:25:11 -0500

    Format Page component for better readability

--- app/spec/page.tsx
+++ app/spec/page.tsx
@@ -1,5 +1,15 @@
+export const dynamic = "force-dynamic";
+
 import Shell from "@/components/Shell";
 import DemoProvider from "@/components/DemoProvider";
 import SpecView from "@/components/views/SpecView";
 
-export default function Page(){return(<DemoProvider><Shell><SpecView/></Shell></DemoProvider>)}
+export default function Page() {
+  return (
+    <DemoProvider>
+      <Shell>
+        <SpecView />
+      </Shell>
+    </DemoProvider>
+  );
+}
