97690f4800ee72e1a78adf66b36b69db99a6c29f
drss <drshweta.shah5@gmail.com>
2026-01-21 11:24:16 -0500

    Delete registry directory

--- registry/page.tsx
+++
@@ -1,5 +0,0 @@
-import Shell from "@/components/Shell";
-import DemoProvider from "@/components/DemoProvider";
-import RegistryView from "@/components/views/RegistryView";
-
-export default function Page(){return(<DemoProvider><Shell><RegistryView/></Shell></DemoProvider>)}
